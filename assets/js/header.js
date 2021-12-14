48e618013f3f7a8be0c7f4aa3003d685bb715887
DaniPoot <danielpoot.uc2@gmail.com>
2021-12-14 09:22:27 -0600

    Add funcionality for navigation

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,24 @@
 const $mainLink = document.querySelector('#header-main-link')
 let $linkSelected = undefined
+const $body = document.querySelector('body')
+const $checkbox = document.querySelector('.navigation-checkbox')
+const $navigationList = document.querySelector('.navigation-list')
 
+$checkbox.addEventListener('change', () => {
+  $body.classList.toggle('overflow-hide')
+})
+
+$navigationList.addEventListener('click', (e) => {
+  const { target } = e
+  const isLink = target.classList.contains('navigation-link')
+  if (isLink) {
+    const activeLink = document.querySelector('.navigation-link-active')
+    if (activeLink) activeLink.classList.toggle('navigation-link-active') 
+    target.classList.toggle('navigation-link-active')
+    $checkbox.checked = false
+    $body.classList.toggle('overflow-hide')
+  }
+})
 
 $mainLink.addEventListener('click', (e) => {
   const { target }  = e
